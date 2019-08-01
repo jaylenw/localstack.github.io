a3ee7a1b90102a19e4a69813a6e25d43f8b118d4
Waldemar Hummer <waldemar.hummer@gmail.com>
2019-08-01 14:37:57 +0200

    latest build

---
+++ app/build/precache-manifest.7b1b111ebffbe084152fb9cb99f092a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bf733307a678a25da66873ab293354d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94601e05280105b3e49b",
+    "url": "/static/css/2.e1d4cefc.chunk.css"
+  },
+  {
+    "revision": "6182812ffa64c7c4f230",
+    "url": "/static/css/main.eeb1651f.chunk.css"
+  },
+  {
+    "revision": "94601e05280105b3e49b",
+    "url": "/static/js/2.67e84d3e.chunk.js"
+  },
+  {
+    "revision": "6182812ffa64c7c4f230",
+    "url": "/static/js/main.6b3de09e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
